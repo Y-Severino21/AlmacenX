f40595fae9a83f042c430ef604004e518ad82d14
Johan809 <2018-6764@itla.edu.do>
2020-02-17 01:13:18 -0400

    Up to Vid 6

--- AlmacenX/Startup.cs
+++ AlmacenX/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using AlmacenX.Data;
 
 namespace AlmacenX
 {
@@ -33,6 +35,9 @@ namespace AlmacenX
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<ClienteContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ClienteContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
