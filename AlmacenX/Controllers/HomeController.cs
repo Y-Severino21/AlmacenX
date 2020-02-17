8188b284a586637ae74be3d09c12dd35d739950b
Y-Severino21 <55861404+Y-Severino21@users.noreply.github.com>
2020-02-16 23:10:54 -0400

    Proyecto Base

--- AlmacenX/Controllers/HomeController.cs
+++ AlmacenX/Controllers/HomeController.cs
@@ -17,14 +17,14 @@ namespace AlmacenX.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sistema De Almacenes";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Detalles de contacto";
 
             return View();
         }
